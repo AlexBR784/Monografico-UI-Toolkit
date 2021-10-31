c6db8322e5afd7c6967a6267cdbc5dc4590b43ba
AlexBR784 <alexbr784@gmail.com>
2021-10-31 19:31:24 +0100

    Initial commit

---
+++ Assets/AñadirMoneda.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AñadirMoneda : MonoBehaviour
+{
+    public Button addCoin;
+    public Label coins;
+    public int monedas;
+
+    
+        
+    
+    private void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        addCoin = root.Q<Button>("AddCoin");
+        coins = root.Q<Label>("label-monedas");
+        monedas = int.Parse(coins.text.Split(' ')[0]);
+        
+        addCoin.RegisterCallback<ClickEvent>(ev=> addCoinToUI());
+
+    }
+
+    private void addCoinToUI(){
+        monedas++;
+        coins.text = $"{monedas} monedas";
+        
+    }
+}
